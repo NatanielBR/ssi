da1fcb26f32d7ae589c9c862c071056f3ddd2f14
Nataniel <natannada08@gmail.com>
2021-02-22 07:28:28 -0300

    Add SSIInfo

---
+++ ssi/src/main/java/br/com/natanielbr/ssi/interfaces/SSIInfo.java
@@ -0,0 +1,6 @@
+package br.com.natanielbr.ssi.interfaces;
+
+public interface SSIInfo {
+	Scrapper[] listScrappers();
+	double getVersion();
+}
--- ssi/src/main/java/br/com/natanielbr/ssi/interfaces/Scrapper.java
+++ ssi/src/main/java/br/com/natanielbr/ssi/interfaces/Scrapper.java
@@ -4,28 +4,23 @@ import br.com.natanielbr.ssi.interfaces.objs.CapituloScan;
 import br.com.natanielbr.ssi.interfaces.objs.MangaInfo;
 import br.com.natanielbr.ssi.interfaces.objs.MangaScan;
 import br.com.natanielbr.ssi.interfaces.utils.MangaScanIterator;
-import okhttp3.*;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 import java.io.Serializable;
 
-public abstract class Scrapper implements Serializable {
+public interface Scrapper extends Serializable {
 
 
-	public abstract String[] findImageInChapter(CapituloScan capituloScan);
+	String[] findImageInChapter(CapituloScan capituloScan);
 
-	public abstract MangaInfo findInfo(MangaScan manga);
+	MangaInfo findInfo(MangaScan manga);
 
 	/**
 	 * Metodo irá até a scan e obter uma lista de mangaScan
 	 *
 	 * @return Uma lista de mangaScan onde irá se atualizar até chegar ao fim na scan.
 	 */
-	public abstract MangaScanIterator listAllManga();
+	MangaScanIterator listAllManga();
 
-	public abstract String getSourceName();
-
-	public abstract String getSourceURL();
+	 String getSourceName();
+	 String getSourceURL();
 }
